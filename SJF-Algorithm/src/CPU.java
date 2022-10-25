b3d4b5d6833c36460992045b5fe2572def6b9eb1
vitordbo <vitorduarte177@gmail.com>
2022-10-24 21:01:01 -0300

    adding comments

--- SJF-Algorithm/src/CPU.java
+++ SJF-Algorithm/src/CPU.java
@@ -3,11 +3,11 @@ public class CPU {
         
         //O tempo de execução de cada processo deve ser gerado aleatoriamente no momento de
         //sua criação
-        Escalonador escalonador = new Escalonador();
+        Escalonador escalonador = new Escalonador(); //Inicia o escalonador:
         Processo processo = null;
-        int tick;
+        int tick;  // o tempo que cada processo deve ter para executar na CPU fictícia é de três segundos
         
-        // 4 processos iniciais
+        // 4 processos iniciais => Quatro processos devem ser criados
         for(int i = 0; i < 4; i++){
             escalonador.inserir(new Processo());
         }
@@ -24,12 +24,12 @@ public class CPU {
 
             escalonador.construirHeap();
 
-            if(processo.getTempoExecucao() > 0)
-            escalonador.inserir(processo);
+            if(processo.getTempoExecucao() > 0){
+                escalonador.inserir(processo);
+            }
         }
 
-
-        // mais 8 processos
+        // mais 8 processos => Após 10 segundos, oito processos devem ser criados.
         for(int i = 0; i < 8; i++){
             escalonador.inserir(new Processo());
         }
@@ -46,12 +46,13 @@ public class CPU {
 
             escalonador.construirHeap();
 
-            if(processo.getTempoExecucao() > 0)
-            escalonador.inserir(processo);
+            if(processo.getTempoExecucao() > 0){
+                escalonador.inserir(processo);
+            }
         }
 
         
-        // mais 16 processos
+        // mais 16 processos => Após 10 segundos, 16 processos devem ser criados.
         for(int i = 0; i < 16; i++){
             escalonador.inserir(new Processo());
         }
@@ -68,9 +69,10 @@ public class CPU {
 
             escalonador.construirHeap();
 
-            if(processo.getTempoExecucao() > 0)
-            escalonador.inserir(processo);
-        }  
+            if(processo.getTempoExecucao() > 0){
+                escalonador.inserir(processo);
+            }
+        }
         
         System.out.println("\n----------- Execução Terminada -----------"); 
     }
